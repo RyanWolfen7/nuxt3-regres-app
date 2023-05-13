e45e71e532b5b78afe4d13a8c40c798b5349bd42
RyanWolfen7 <wolfen7lada@gmail.com>
2023-05-13 01:15:05 -0500

    minor style changes: NOTE: couldnt disable google autocomplete with tailwind

--- pages/join/index.vue
+++ pages/join/index.vue
@@ -32,9 +32,9 @@
     <div class="flex items-center justify-center h-screen">
         <div class="bg-secondary p-4">
             <form @submit.prevent="handleSubmit" class="text-center">
-                <input type="text" v-model="form.userName" placeholder="Username" class="w-full mb-2 bg-accent text-secondary py-2 px-4 rounded">
-                <input type="email" v-model="form.email" placeholder="Email" class="w-full mb-2 bg-accent text-secondary py-2 px-4 rounded">
-                <input type="password" v-model="form.password" placeholder="Password" class="w-full mb-2 bg-accent text-secondary py-2 px-4 rounded">
+                <input type="text" v-model="form.userName" placeholder="Username" class="w-full mb-2 bg-secondary !important text-primary py-2 px-4 rounded" autocomplete="off">
+                <input type="email" v-model="form.email" placeholder="Email" class="w-full mb-2 bg-secondary !important text-primary py-2 px-4 rounded" autocomplete="off">
+                <input type="password" v-model="form.password" placeholder="Password" class="w-full mb-2 bg-secondary !important text-primary py-2 px-4 rounded" autocomplete="off">
                 <button type="submit" class="w-full bg-main text-secondary py-2 px-4 rounded">Join</button>
             </form>
         </div>
