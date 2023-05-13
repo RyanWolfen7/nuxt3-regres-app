986de0302bc847975203f0d9081c32703583ff2c
RyanWolfen7 <wolfen7lada@gmail.com>
2023-05-13 13:58:22 -0500

    set up filterUsers to successfully paginate and return the expected response

--- server/utils/reqres.ts
+++ server/utils/reqres.ts
@@ -1,16 +1,23 @@
 import axios from 'axios'
 import { User, GetUserResponse } from '~~/types';
 
-export async function filterUsers( key: string, value: any, pageNumber: number = 1) {
+type FilterUsersReturnType = boolean | ((key: string, value: any, pageNumber?: number) => Promise<boolean>);
+
+export async function filterUsers( key: string, value: any, pageNumber: number = 1): Promise<FilterUsersReturnType> {
     const { data: response } = await axios.get<GetUserResponse>(`https://reqres.in/api/users?page=${pageNumber}`)
     const { data, page, total_pages: totalPages } = response;
+    let resValue: boolean | Function = false
     if (!data) return true;
     const existingUser = data.find((user: User ) => user[key] == value);
     if(existingUser) {
         return true;
-    } else if (page !== totalPages) {
-        filterUsers(value, pageNumber + 1);
-    } else {
-        return false;
     }
+    if (page !== totalPages) {
+        resValue = await filterUsers(key, value, pageNumber + 1);
+    }
+
+    if (typeof resValue === 'function') {
+        return resValue(key, value, pageNumber);
+    }
+    return resValue;
 }
