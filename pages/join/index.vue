22b45ea83598909f420f21aecd57aa45b7f75911
RyanWolfen7 <wolfen7lada@gmail.com>
2023-05-13 01:06:22 -0500

    setup join page and route. TODO: add validation, success/failure state and implement the route connection to reqres

--- pages/join/index.vue
+++ pages/join/index.vue
@@ -1,5 +1,42 @@
+<script lang="ts">
+    export default {
+        name: 'Join',
+        data() {
+            return {
+                form: {
+                    userName: '',
+                    email: '',
+                    password: ''
+                },
+            }
+        },
+        methods: {
+            async handleSubmit() {
+                const { data: response } = await useFetch('/api/v1/user/create', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.form)
+                })
+
+                if (response) {
+                    alert(response.value);
+                }
+            }
+        }
+    }
+</script>
+
 <template>
-    <div>
-        <h1> join </h1>
+    <div class="flex items-center justify-center h-screen">
+        <div class="bg-secondary p-4">
+            <form @submit.prevent="handleSubmit" class="text-center">
+                <input type="text" v-model="form.userName" placeholder="Username" class="w-full mb-2 bg-accent text-secondary py-2 px-4 rounded">
+                <input type="email" v-model="form.email" placeholder="Email" class="w-full mb-2 bg-accent text-secondary py-2 px-4 rounded">
+                <input type="password" v-model="form.password" placeholder="Password" class="w-full mb-2 bg-accent text-secondary py-2 px-4 rounded">
+                <button type="submit" class="w-full bg-main text-secondary py-2 px-4 rounded">Join</button>
+            </form>
+        </div>
     </div>
 </template>
