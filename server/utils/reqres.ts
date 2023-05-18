f2edd075a9c1585f0e7b1fced17cd0c8f7da5b56
RyanWolfen7 <wolfen7lada@gmail.com>
2023-05-18 15:23:48 -0500

    composed getExistingUser and getUser

--- server/utils/reqres.ts
+++ server/utils/reqres.ts
@@ -1,19 +1,21 @@
 import axios from 'axios'
 import { User, GetUserResponse, ReturnTypes } from '~~/types';
 
-const getUsers = async (pageNumber: number) => {
+var getUsers = async (pageNumber: number) => {
     const { data } = await axios.get<GetUserResponse>(`https://reqres.in/api/users?page=${pageNumber}`)
     return data
 }
 
-const getExistingUser = async (key: string, value: string, response: any): Promise<User | null> => {
+var getExistingUser = async (key: string, value: string, response: any): Promise<User | null> => {
     const { data, page, total_pages: totalPages } = await response;
     if (!data) return null
     const user = data.find((user: User) => user[key] == value)
     if (user) return user
-    if (page !== totalPages) { return getExistingUser(key, value, getUsers(page + 1)) }
+    if (page !== totalPages) { return paginateUserResponse(key, value, page + 1) }
     return null
 }
+
+var paginateUserResponse = (key: string, value: string, page: number) => getExistingUser(key, value, getUsers(page)) 
 
 export async function filterUsers(key: string, value: any, pageNumber: number = 1): Promise<User | null> {
     const usersData = await getUsers(pageNumber)
